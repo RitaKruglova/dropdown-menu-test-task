c913bc82912347a6896b04d098fda8aead5fb5d2
RitaKruglova <kruglova404@mail.ru>
2024-07-31 20:41:54 +0300

    feat: add styles for dropdown-menu

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,49 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import appStyles from './app.module.css';
 import MoreButton from '../more-button/more-button';
+import DropdownMenu from '../dropdown-menu/dropdown-menu';
+import MenuItem from '../menu-item/menu-item';
+import shareIconPath from '../../images/share.svg';
+import editIconPath from '../../images/edit.svg';
+import trashIconPath from '../../images/trash.svg';
 
 const App: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  function handleClick(): void {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    } else {
+      setIsMenuOpen(true);
+    }
+  }
+
   return (
     <div className={appStyles.container}>
-      <MoreButton />
-      <MoreButton />
-      <MoreButton />
+      <div className={appStyles.buttons}>
+        <MoreButton handleClick={handleClick} />
+        <MoreButton handleClick={handleClick} />
+        <MoreButton handleClick={handleClick} />
+      </div>
+      {isMenuOpen &&
+        <DropdownMenu>
+          <MenuItem
+            text="Поделиться в социальных сетях"
+            iconPath={shareIconPath}
+            alt="Поделиться"
+          />
+          <MenuItem
+            text="Редактировать страницу"
+            iconPath={editIconPath}
+            alt="Редактировать"
+          />
+          <MenuItem
+            text="Удалить страницу"
+            iconPath={trashIconPath}
+            alt="Удалить"
+          />
+        </DropdownMenu>
+      }
     </div>
   );
 };
--- src/components/more-button/more-button.tsx
+++ src/components/more-button/more-button.tsx
@@ -2,12 +2,17 @@ import { FC } from 'react';
 import moreButtonStyles from './more-button.module.css';
 import moreIconPath from '../../images/more.svg';
 
-const MoreButton: FC = () => {
+interface IMoreButtonProps {
+  handleClick: () => void;
+}
+
+const MoreButton: FC<IMoreButtonProps> = ({ handleClick }) => {
   return (
     <button
       className={moreButtonStyles.button}
       type="button"
       aria-label="Открыть меню"
+      onClick={handleClick}
     >
       <img className={moreButtonStyles.icon} src={moreIconPath} alt="Открыть меню" />
     </button>
